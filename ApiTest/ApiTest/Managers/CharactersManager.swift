87bada1b8215f7930cf8ed83a4e87d11b8937602
vwf <vwff@icloud.com>
2020-12-26 22:54:23 +0500

    HTTP REST homework

---
+++ ApiTest/ApiTest/Managers/CharactersManager.swift
@@ -0,0 +1,48 @@
+//
+//  CharactersManager.swift
+//  ApiTest
+//
+//  Created by FARIT GATIATULLIN on 26.12.2020.
+//
+
+import Foundation
+
+//var allCharacters:[CharacterResult?]? = []
+
+var characters = [CharacterResult]()
+
+class CharactersManager {
+    static let shared = CharactersManager()
+    
+    private init() {}
+    
+    func fetchCharacter(closure: ((CharacterResult?) -> Void)?) {
+        RequestHandler(path: "people")
+            .response { (model: [CharacterResult]?) in
+                print("Model fetched: \(model)")
+                //closure?(model?)
+                //allCharacters!.append(contentsOf: model!)
+            }
+            .responseJSON { (json) in
+                print("JSON fetched: \(json)")
+            }
+    }
+
+    func fetchAllCharacters(closure: (([CharacterResult]?) -> Void)?) {
+        
+        RequestHandler(path: "people").response(closure)
+         
+    }
+    
+    func fetchAbsolutelyAllCharacters(closure: (([CharacterResult]?) -> Void)?) {
+        for i in 1...8 {
+        RequestHandler(path: "people/?page=\(i)").response(closure)
+           
+        }
+        
+//        Test
+        
+        
+        
+    }
+}
